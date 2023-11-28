2c8d93297bfe2fbac87bbfbd21dead2acaeffa6f
fhxy824 <fhxy824@163.com>
2023-11-28 14:02:51 +0800

    change for docker hub2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ password = os.environ['POSTGRES_PASSWORD']
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker! I juset changed it with github. see main branch'
+    return 'Hello, Docker! I juset changed it with github. see main branch oh, change again and again'
 
 
 @app.route('/widgets')
